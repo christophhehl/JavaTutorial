dd11dd40ebe841061183f5379af214799dacfd1f
Christoph Hehl <christophhehl363@gmail.com>
2024-08-27 00:41:13 +0200

    synchronized comments in exercise and solution 1

--- src/Solution1.java
+++ src/Solution1.java
@@ -2,6 +2,7 @@ public class Solution1 {
 
     public static void main(String[] args) {
         // print the text "Hello World"
+        // you can print text by writing System.out.println()
         System.out.println("Hello World");
     }
 
